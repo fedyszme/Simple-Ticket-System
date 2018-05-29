e14ed6f77dffdfa2a562a3833f56fb38ff22e0d1
fedyszme <36646062+fedyszme@users.noreply.github.com>
2018-05-29 09:36:20 -0400

    Added files

---
+++ Controllers/TicketsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SimpleTicketSystem.Data;
+using SimpleTicketSystem.Models;
+
+namespace SimpleTicketSystem.Controllers
+{
+    
+    public class TicketsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TicketsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tickets
+        public async Task<IActionResult> Index()
+        {
+            var openTickets = await _context.tickets.Where(x => x.Closed == false).ToListAsync();
+
+            return View(openTickets);
+        }
+
+        // GET: Tickets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.tickets
+                .SingleOrDefaultAsync(m => m.TicketId == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // GET: Tickets/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tickets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TicketId,Name,Description")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                var userName = HttpContext.User.Identity.Name;
+                ticket.OpenedBy = userName;
+
+                _context.Add(ticket);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ticket);
+        }
+
+        // GET: Tickets/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.tickets.SingleOrDefaultAsync(m => m.TicketId == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: Tickets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TicketId,Name,Description,Closed, OpenedBy")] Ticket ticket)
+        {
+            if (id != ticket.TicketId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (ticket.Closed == true)//so the if loop sets the closedBy to the users name, but it re-writes the whole instance so you lose your OpenedBy
+                        //by adding to the edit view, html code of a hidden form you can pass on the OpenBy just like it already was set up for TicketId
+                    {
+                        var userName = HttpContext.User.Identity.Name;
+                        ticket.ClosedBy = userName;
+
+                    }
+                        _context.Update(ticket);
+                        await _context.SaveChangesAsync();
+  
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TicketExists(ticket.TicketId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ticket);
+        }
+
+        // GET: Tickets/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.tickets
+                .SingleOrDefaultAsync(m => m.TicketId == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ticket = await _context.tickets.SingleOrDefaultAsync(m => m.TicketId == id);
+            _context.tickets.Remove(ticket);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TicketExists(int id)
+        {
+            return _context.tickets.Any(e => e.TicketId == id);
+        }
+    }
+}
---
+++ Models/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTicketSystem.Models
+{
+    public class Ticket
+    {
+
+        public int TicketId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string OpenedBy { get; set; }
+        [ScaffoldColumn(false)]
+        public string ClosedBy { get; set; }
+
+        public bool Closed { get; set; }
+
+       
+    }
+}
